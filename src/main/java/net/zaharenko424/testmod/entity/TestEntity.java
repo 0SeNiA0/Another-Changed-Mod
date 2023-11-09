c39d11971444aa42241288017f96cfbb1f38ef5c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 20:19:42 +0100

    test entity update

--- src/main/java/net/zaharenko424/testmod/entity/TestEntity.java
+++ src/main/java/net/zaharenko424/testmod/entity/TestEntity.java
@@ -2,31 +2,38 @@ package net.zaharenko424.testmod.entity;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
-public class TestEntity extends PathfinderMob {
-    public TestEntity(EntityType<? extends PathfinderMob> p_21368_, Level p_21369_) {
+public class TestEntity extends Monster {
+    public TestEntity(EntityType<? extends Monster> p_21368_, Level p_21369_) {
         super(p_21368_, p_21369_);
     }
 
     public static AttributeSupplier.@NotNull Builder createAttributes(){
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH,20).add(Attributes.MOVEMENT_SPEED, 0.25);
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH,20)
+                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.FOLLOW_RANGE, 35.0)
+                .add(Attributes.ATTACK_DAMAGE, 4.0)
+                .add(Attributes.ARMOR, 2.0);
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new RandomStrollGoal(this,1));
-        this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0, false));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
     }
 }
