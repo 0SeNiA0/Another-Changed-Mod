48465f7bb46444deba2051115f1cdfefda05f698
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 22:03:32 +0100

    ai updates

---
+++ src/main/java/net/zaharenko424/testmod/entity/AbstractLatexBeast.java
@@ -0,0 +1,48 @@
+package net.zaharenko424.testmod.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.zaharenko424.testmod.TransfurManager;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class AbstractLatexBeast extends Monster {
+    public final String transfurType;
+
+    protected AbstractLatexBeast(EntityType<? extends Monster> p_33002_, Level p_33003_, String transfurType) {
+        super(p_33002_, p_33003_);
+        this.transfurType=transfurType;
+    }
+
+    protected static AttributeSupplier.@NotNull Builder baseAttributes(){
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH,20)
+                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.FOLLOW_RANGE, 35.0)
+                .add(Attributes.ATTACK_DAMAGE, 4.0)
+                .add(Attributes.ARMOR, 2.0);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(2, new LatexAttackGoal(this, 1.0, false));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true, livingEntity -> !(livingEntity instanceof Player player&&TransfurManager.isTransfurred(player))));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
+    }
+}
