0338408bcb581f0e1f49f5b75afd877636e17744
Cseh Dominik <cseh.dominik05@gmail.com>
2025-11-17 07:45:17 +0100

    Update hu_hu

--- src/main/java/net/zaharenko424/a_changed/datagen/lang/HULanguageProvider.java
+++ src/main/java/net/zaharenko424/a_changed/datagen/lang/HULanguageProvider.java
@@ -367,22 +367,22 @@ public class HULanguageProvider extends LanguageProvider {
         addTooltip("wires", "Áteresztőképesség: %1$s EU/t");
 
         //Transfurs
-        add("transfur.a_changed.bei_feng", "Pejfeng");
-        add("transfur.a_changed.benign", "Jóságos latex");
-        add("transfur.a_changed.dark_latex_pup", "Sötét latex farkaskölyök");
-        add("transfur.a_changed.dark_latex_wolf_female", "Nőstény sötét latex farkas");
-        add("transfur.a_changed.dark_latex_wolf_male", "Hím sötét latex farkas");
-        add("transfur.a_changed.gas_wolf", "Gáz farkas");
-        add("transfur.a_changed.hypno_cat", "Hipnómacska");
-        add("transfur.a_changed.latex_shark_female", "Nőstény latex cápa");
-        add("transfur.a_changed.latex_shark_male", "Hím latex cápa");
-        add("transfur.a_changed.pure_white_latex_wolf", "Tiszta fehér latex farkas");
-        add("transfur.a_changed.snow_leopard_female", "Nőstény hópárduc");
-        add("transfur.a_changed.snow_leopard_male", "Hím hópárduc");
-        add("transfur.a_changed.special", "Speciális");
-        add("transfur.a_changed.white_latex_pup", "Fehér latex farkaskölyök");
-        add("transfur.a_changed.white_latex_wolf_female", "Nőstény fehér latex farkas");
-        add("transfur.a_changed.white_latex_wolf_male", "Hím fehér latex farkas");
-        add("transfur.a_changed.yufeng_dragon", "Jüfeng sárkány");
+        add("transfur." + modid + ".bei_feng", "Pejfeng");
+        add("transfur." + modid + ".benign", "Jóságos latex");
+        add("transfur." + modid + ".dark_latex_pup", "Sötét latex farkaskölyök");
+        add("transfur." + modid + ".dark_latex_wolf_female", "Nőstény sötét latex farkas");
+        add("transfur." + modid + ".dark_latex_wolf_male", "Hím sötét latex farkas");
+        add("transfur." + modid + ".gas_wolf", "Gáz farkas");
+        add("transfur." + modid + ".hypno_cat", "Hipnómacska");
+        add("transfur." + modid + ".latex_shark_female", "Nőstény latex cápa");
+        add("transfur." + modid + ".latex_shark_male", "Hím latex cápa");
+        add("transfur." + modid + ".pure_white_latex_wolf", "Tiszta fehér latex farkas");
+        add("transfur." + modid + ".snow_leopard_female", "Nőstény hópárduc");
+        add("transfur." + modid + ".snow_leopard_male", "Hím hópárduc");
+        add("transfur." + modid + ".special", "Speciális");
+        add("transfur." + modid + ".white_latex_pup", "Fehér latex farkaskölyök");
+        add("transfur." + modid + ".white_latex_wolf_female", "Nőstény fehér latex farkas");
+        add("transfur." + modid + ".white_latex_wolf_male", "Hím fehér latex farkas");
+        add("transfur." + modid + ".yufeng_dragon", "Jüfeng sárkány");
     }
 }
